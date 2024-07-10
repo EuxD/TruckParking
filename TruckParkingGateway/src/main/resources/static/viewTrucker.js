b11f2cfd8559187aebb303b7543144f2260dab70
Antonello-01 <80790317+Antonello-01@users.noreply.github.com>
2024-07-10 19:41:21 +0200

    Implementazione tramite ID viewTrucker.js

--- TruckParkingGateway/src/main/resources/static/viewTrucker.js
+++ TruckParkingGateway/src/main/resources/static/viewTrucker.js
@@ -2,8 +2,8 @@ document.addEventListener("DOMContentLoaded", function() {
     document.getElementById("truckerForm").addEventListener("submit", function(event) {
         event.preventDefault(); // Prevent form from submitting the traditional way
 
-        var email = document.getElementById("email").value;
-        var trucker = handleTruckerRequestByEmail(email);
+        var id = document.getElementById("id").value;
+        var trucker = handleTruckerRequestById(id);
 
         if (trucker) {
             buildList([trucker]);
@@ -30,7 +30,10 @@ function buildListItem(trucker, number) {
     const li = document.createElement("li");
     li.style.setProperty("--i", String(number));
 
-
+    currentElement = document.createElement("h5");
+    currentElement.innerText = "ID: " + trucker.id_trucker;
+    currentElement.style.fontWeight = "bold"; // Applico un font-weight in più per evidenziare l'ID
+    li.appendChild(currentElement);
 
     var currentElement = document.createElement("h5");
     currentElement.setAttribute("class", "name-class");
@@ -42,33 +45,18 @@ function buildListItem(trucker, number) {
     currentElement.style.fontWeight = "bold";
     li.appendChild(currentElement);
 
-    currentElement = document.createElement("h5");
-    currentElement.innerText = "ID: " + trucker.id_trucker;
-    currentElement.style.fontWeight = "bold"; // Applico un font-weight in più per evidenziare l'ID
-    li.appendChild(currentElement);
-
-    currentElement = document.createElement("h5");
-    currentElement.setAttribute("class", "email-class");
-    currentElement.innerText = "Email: " + trucker.email;
-    li.appendChild(currentElement);
-
-    currentElement = document.createElement("h5");
-    currentElement.innerText = "Date of Birth: " + trucker.bDate;
-    li.appendChild(currentElement);
 
     currentElement = document.createElement("h5");
     currentElement.setAttribute("class", "role-class");
     currentElement.innerText = "Role: " + trucker.role;
     li.appendChild(currentElement);
 
-
-
     return li;
 }
 
-function handleTruckerRequestByEmail(email) {
+function handleTruckerRequestById(id) {
     var xhttp = new XMLHttpRequest();
-    var url = 'http://localhost:8080/truckparking/rest/trucker/email/' + email;
+    var url = 'http://localhost:8080/truckparking/rest/trucker/ID/' + id;
     xhttp.open("GET", url, false);
     xhttp.send();
 
@@ -82,5 +70,5 @@ function handleTruckerRequestByEmail(email) {
 
 function displayNoResults() {
     const resultsDiv = document.getElementById("results");
-    resultsDiv.innerHTML = "<p>No trucker found with the provided email.</p>";
+    resultsDiv.innerHTML = "<p>No trucker found with the provided ID.</p>";
 }
